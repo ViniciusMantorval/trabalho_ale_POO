467036ac79387858e541ddcc9f2888a2d4be28e1
Vini <VMGMaster0.com>
2026-09-17 21:07:13 -0300

    adiciona classe televisão e 90 porcento do teste da televisao no main

--- Main.java
+++ Main.java
@@ -6,6 +6,8 @@ public class Main{
 		System.out.println();	
 		TesteLivro();
 		System.out.println();	
+		TesteTelevisao();
+		System.out.println();	
 	}
 
 	public static void TesteCaneta(){
@@ -30,5 +32,19 @@ public class Main{
 	livro2.Abrir();	
 	livro2.Ler();	
 	System.out.println(livro1.genero);	
+	}
+	public static void TesteTelevisao(){
+		int[] dimensoes = {20,60};
+		Televisao televisao1 = new Televisao("Sansung",dimensoes,true,2);
+		televisao1.Sintonizar(420);
+		televisao1.Ligar();
+		televisao1.Sintonizar(420);
+		System.out.println(televisao1.quantidadePortasHDMI);	
+
+		Televisao televisao2 = new Televisao("Sansung",dimensoes,true,2);
+		televisao2.Sintonizar(67);
+		televisao2.Ligar();
+		televisao2.Sintonizar(67);
+		System.out.println(televisao2.quantidadePortasHDMI);	
 	}
 }
---
+++ model/Televisao.java
@@ -0,0 +1,28 @@
+public class Televisao{
+	public String marca;
+	public int[] dimensoes;
+	public boolean isSmart;
+	public int quantidadePortasHDMI;
+	boolean ligado= false;
+
+	public Televisao(String marca,int[] dimensoes,boolean isSmart, int quantidadePortasHDMI){
+		this.marca = marca;
+		this.dimensoes = dimensoes;
+		this.quantidadePortasHDMI = quantidadePortasHDMI;
+	}
+	public void Ligar(){
+		if(ligado){
+			System.out.println("A televisão "+ this.marca + "já está ligada");
+		}else{
+			System.out.println("A televisão "+ this.marca + "ligada");
+			ligado = true;
+		}
+	}
+	public void Sintonizar(int canal){
+		if(ligado){
+			System.out.println("A televisão "+ this.marca + "sintonizando no canal "+ canal);
+		}else{
+			System.out.println("a televisão "+ this.marca + "está desligada, impossivel sintonizar");
+		}
+	}
+}
